07f32626e81800601c3d4f187caf47ad6dc41b07
Ian Chu Te <ianchute@hotmail.com>
2017-08-05 21:41:54 +0800

    tags

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'toady',
   packages = ['toady'], # this must be the same as the name above
-  version = '1.00',
+  version = '1.10',
   description = 'Easily visualize high-dimensional data in 2d space',
   author = 'Ian Chu Te',
   author_email = 'ianchute@hotmail.com',
   url = 'https://github.com/ianchute/toady', # use the URL to the github repo
-  download_url = 'https://github.com/ianchute/toady/archive/1.00.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/ianchute/toady/archive/1.10.tar.gz', # I'll explain this in a second
   keywords = ['visualization', 'embedding', 'data science', 'data'], # arbitrary keywords
   classifiers = [],
   install_requires=[
