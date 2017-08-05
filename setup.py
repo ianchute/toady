8788686fccfcb9bc3835e416b2a9bc55b3c2b60d
Ian Chu Te <ianchute@hotmail.com>
2017-08-05 19:11:12 +0800

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'toady',
+  packages = ['toady'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Easily visualize high-dimensional data in 2d space',
+  author = 'Ian Chu Te',
+  author_email = 'ianchute@hotmail.com',
+  url = 'https://github.com/ianchute/toady', # use the URL to the github repo
+  download_url = 'https://github.com/ianchute/toady/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['visualization', 'embedding', 'data science', 'data'], # arbitrary keywords
+  classifiers = [],
+)
---
+++ toady/__init__.py
@@ -0,0 +1,1 @@
+import toady
