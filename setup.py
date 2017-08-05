4fde6d4a9f0dd705e8bab45057d338c19ed1f036
Ian Chu Te <ianchute@hotmail.com>
2017-08-05 19:34:49 +0800

    Fix tag mistake.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'toady',
   packages = ['toady'], # this must be the same as the name above
-  version = '0.21',
+  version = '0.25',
   description = 'Easily visualize high-dimensional data in 2d space',
   author = 'Ian Chu Te',
   author_email = 'ianchute@hotmail.com',
   url = 'https://github.com/ianchute/toady', # use the URL to the github repo
-  download_url = 'https://github.com/ianchute/toady/archive/0.21.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/ianchute/toady/archive/0.25.tar.gz', # I'll explain this in a second
   keywords = ['visualization', 'embedding', 'data science', 'data'], # arbitrary keywords
   classifiers = [],
 )
---
+++ toady/__init__.py
@@ -0,0 +1,1 @@
+from .toady import toady
